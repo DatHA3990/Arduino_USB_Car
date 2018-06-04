0767fcaddcf056dd6cf741c32da0b09b2b27c067
DatHA3990 <datfalcon@gmail.com>
2018-06-04 19:52:36 -0400

    optimised algo

--- Controller.py
+++ Controller.py
@@ -1,29 +1,30 @@
-# Controller for Arduino car
-# by Dat HA on May 25th 2018
+import pygame, time, serial
 
-import pygame               # import pygame library for joystick controls
-from Communication import * # import our Arduino-PC communication class
+pygame.display.init()
+pygame.joystick.init()
 
-pygame.display.init()  # start pygame display (no actual use, just necessary)
-pygame.joystick.init() # start a joystick
+joystick = pygame.joystick.Joystick(0).init()
 
-joystick = pygame.joystick.Joystick(0).init() # initiate joystick
+null_zone = 0.15
 
-null_zone = 0.15 # set zone at which joystick will not react
+arduino = serial.Serial('COM3', 9600)
 
-arduino = Arduino('COM3') # start communication with Arduino
-
+prev = time.time()
     
-while True: # infinite loop until the communication is severed
-    pygame.event.pump() # update joystick
+while True:
+    pygame.event.pump()
 
-    direction = pygame.joystick.Joystick(0).get_axis(2) # get joystick direction value
-    speed = -pygame.joystick.Joystick(0).get_axis(1)    # get joystick speed value
+    direction = pygame.joystick.Joystick(0).get_axis(2)
+    speed = -pygame.joystick.Joystick(0).get_axis(1)
 
-    direction = direction * (abs(direction) > null_zone) # if direction value is in null zone, set it to 0
-    speed = speed * (abs(speed) > null_zone)             # if speed value is in null zone, set it to 0
+    direction = direction * (abs(direction) > null_zone)
+    speed = speed * (abs(speed) > null_zone)
 
 
-    motor = [int(max(min(1, speed + (direction * [1, -1][i])), -1) * 255) for i in range(2)] # translate direction and speed value to motor value for tank drive
+    motor = [int(max(min(1, speed + (direction * [1, -1][i])), -1) * 255) for i in range(2)]
 
-    arduino.update(str(motor[0])+','+str(motor[1])) # send if to the Arduino
+    if time.time() - prev > 0.02:
+        arduino.write(bytes([abs(motor[0]),abs(motor[1]), motor[0]<0, motor[1]<1]))
+        prev = time.time()
+
+
