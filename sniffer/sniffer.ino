d0c50a56bcc90fd84d43aab6187ee6de4cfbc9a0
DatHA3990 <datfalcon@gmail.com>
2018-06-24 09:34:52 -0400

    working on sniffer

--- sniffer/sniffer.ino
+++ sniffer/sniffer.ino
@@ -1,4 +1,4 @@
- #include <SPI.h>
+#include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
 
@@ -7,6 +7,8 @@ RF24 radio (7, 8);
 const int transmitterAdress = 0xF0F0F0F0AA;
 const int receiverAdress = 0xF0F0F0F0BB;
 
+byte pipeTx = 1;
+byte pipeRx = 2;
 
 byte dataTrasmitter[2];
 byte dataReceiver[5];
@@ -15,16 +17,17 @@ void setup() {
   Serial.begin(9600);
 
   radio.begin();
-  radio.openReadingPipe(0, transmitterAdress);
-  radio.openReadingPipe(1, receiverAdress);
-  radio.setPALevel(RF24_PA_MIN);
+  
+  radio.openReadingPipe(pipeTx, transmitterAdress);
+  radio.openReadingPipe(pipeRx, receiverAdress);
   radio.startListening();
+  
+  radio.setPALevel(RF24_PA_MIN);
   Serial.println("test");
 }
 
 void loop() {
-  
-  if (radio.available(1)) {
+  if (radio.available(&pipeTx)) {
     radio.read(&dataTrasmitter, sizeof(dataTrasmitter));
 
     Serial.print("From Tx : ");
@@ -35,7 +38,8 @@ void loop() {
     Serial.println();
   }
 
-  if (radio.available(0)) {
+
+  if (radio.available(&pipeRx)) {
     radio.read(&dataReceiver, sizeof(dataReceiver));
 
     Serial.print("From Rx : ");
